f4ef7c26b49191e02abeb50c0a6c72469c3fb831
Tamara Jarvis <tamara.jarvis17@icloud.com>
2020-12-16 02:47:17 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -28,16 +28,3 @@ function sayHiToGrandma (string) {
     return mixedcase
   }
 }
-
-
-  
-  if (string) = lowercase {
-  return "I can't hear you!";
-  }
-  if (string) = uppercase {
-  return "YES INDEED!";
-  }
-  if (string) = "I love you, Grandma." {
-  return "I love you, too.";
-  }
-}
